95831f8a92c4ec71cf543766f386ca176d56d959
KtoYkraJlMoyNick <nktrsk@gmail.com>
2016-01-10 23:52:15 +0300

    fix slider

--- src/js/common.js
+++ src/js/common.js
@@ -1,23 +1,20 @@
 $(document).ready(function(){
     $(".main-nav__tab:first-child a").click(function(event){
         event.preventDefault();
-        $(".main-nav__link--active").removeClass("main-nav__link--active");
-        $(this).addClass("main-nav__link--active");
-        $(".slider__show").removeClass("slider__show");
-        $(".slider__slide:first-child").addClass("slider__show");
+        activate(".main-nav__tab:first-child a", ".main-nav__link--active", "main-nav__link--active" );
+        showSlide(".slider__slide:first-child", ".slider__show", "slider__show");
     });
     $(".main-nav__tab:nth-child(2) a").click(function(event){
         event.preventDefault();
-        $(".main-nav__link--active").removeClass("main-nav__link--active");
-        $(this).addClass("main-nav__link--active");
-        $(".slider__show").removeClass("slider__show");
-        $(".slider__slide:nth-child(2)").addClass("slider__show");
+        activate(".main-nav__tab:nth-child(2) a", ".main-nav__link--active", "main-nav__link--active" );
+        showSlide(".slider__slide:nth-child(2)", ".slider__show", "slider__show");
     });
-    $(".main-nav__tab:nth-child(3) a").click(function(event){
-        event.preventDefault();
-        $(".main-nav__link--active").removeClass("main-nav__link--active");
-        $(this).addClass("main-nav__link--active");
-        $(".slider__show").removeClass("slider__show");
-        $(".slider__slide:nth-child(3)").addClass("slider__show");
-    });
+    var activate = function(target, elem, activeElem){
+        $(elem).removeClass(activeElem);
+        $(target).addClass(activeElem);
+    };
+    var showSlide = function(target, elem, showElem){
+        $(elem).removeClass(showElem);
+        $(target).addClass(showElem);
+    };
 });
