19d741600192c04820296db7bf456179b6db7ad0
KtoYkraJlMoyNick <nktrsk@gmail.com>
2016-02-21 18:33:25 +0300

    Pink

--- pink/js/script.js
+++ pink/js/script.js
@@ -11,6 +11,9 @@
         var xhr = new XMLHttpRequest( ); 
         var time = (new Date( )).getTime( ); 
         xhr.open("post", "https://echo.htmlacademy.ru/adaptive?" + time); 
+        que.forEach(function(element){
+            data.append("images", element.file);
+        }); 
         xhr.addEventListener("readystatechange", function( ) { 
             if (xhr.readyState == 4) { 
 					console.log(xhr.responseText);
@@ -18,9 +21,52 @@
                 } 
             }); 
         xhr.send(data); 
-    }) 
+    }); 
     closeModal.addEventListener("click", function(event){
         event.preventDefault();
         successModal.classList.remove("modal-content--show");
-    })
+    });
+    if ("FileReader" in window) {
+        var uploadImgBtn = form.querySelector("#file-upload"),
+            que = [];
+            uploadImgBtn.addEventListener("change", function(){
+                var files = this.files;
+                for (var i=0; i<files.length;i++) {
+                    preview(files[i]);
+                }
+                this.value = "";
+            });
+        function preview(file) {
+            if ( file.type.match(/image.*/)) {
+                var reader = new FileReader();
+                reader.addEventListener("load", function(event){
+                    var template = document.querySelector("#template").innerHTML,
+                        parent = form.querySelector(".photos-form__attached");
+                    Mustache.parse(template);
+                    var html = Mustache.render(template, {
+                        "source": event.target.result,
+                        "name":   file.name
+                    });
+                    var div = document.createElement("div");
+                    div.classList.add("photos-form__item");
+                    div.innerHTML = html; 
+                    parent.appendChild(div);
+                    div.querySelector(".photos-form__remove-photo").addEventListener("click", function() {
+                        removePreview(div);
+                    });
+                    que.push({
+                        "file": file,
+                        "div": div
+                    });
+                })
+                reader.readAsDataURL(file);
+            }
+        }
+        function removePreview(div) {
+            que = que.filter(function(element) {
+                return element.div != div;
+            });
+            div.parentNode.removeChild(div);
+        }   
+    }
 })();
